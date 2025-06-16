04d88e9e525fbd92b6c919d0101cdcc85af5a3fa
kiranchaudry97 <kiran.chaudry@outlook.be>
2025-06-16 19:46:35 +0200

    routes aanpassesn

--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -4,15 +4,16 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(_DIR_))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: _DIR_.'/../routes/web.php',
+        commands: _DIR_.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Voeg hier middleware toe indien nodig
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+        // Voeg hier exception handlers toe indien nodig
+    })
+    ->create();
